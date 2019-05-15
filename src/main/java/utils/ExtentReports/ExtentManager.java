d494f5bdc2e2bfb0122f7263f269d9e626ee6a29
Iuliia Sviderska <liazhur@gmail.com>
2019-05-15 00:43:59 -0400

    File.separator added to ExtentReports

--- src/main/java/utils/ExtentReports/ExtentManager.java
+++ src/main/java/utils/ExtentReports/ExtentManager.java
@@ -1,19 +1,22 @@
 package utils.ExtentReports;
 
+import java.io.File;
+
 import com.relevantcodes.extentreports.ExtentReports;
 
 //OB: ExtentReports extent instance created here. That instance can be reachable by getReporter() method.
 
 public class ExtentManager {
 
-    private static ExtentReports extent;
+	private static ExtentReports extent;
 
-    public synchronized static ExtentReports getReporter(){
-        if(extent == null){
-            //Set HTML reporting file location
-            String workingDir = System.getProperty("user.dir");
-            extent = new ExtentReports(workingDir+"\\ExtentReports\\ExtentReportResults.html", true);
-        }
-        return extent;
-    }
+	public synchronized static ExtentReports getReporter() {
+		if (extent == null) {
+			// Set HTML reporting file location
+			String workingDir = System.getProperty("user.dir");
+			extent = new ExtentReports(
+					workingDir + File.separator + "ExtentReports" + File.separator + "ExtentReportResults.html", true);
+		}
+		return extent;
+	}
 }
